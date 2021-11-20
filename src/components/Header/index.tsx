784ff871fc100ae25a9e1a5101b27a8342b0c7ef
Marcelo cerqueira <marcelocerqueira661@gmail.com>
2021-11-20 00:03:52 -0300

    creaçao de interface do dt-maney

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import logoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+
+export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState();
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt=" dt money" />
+        <button type="button">Nova transação</button>
+      </Content>
+    </Container>
+  );
+}
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,34 @@
+import inconeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entrada</p>
+          <img src={inconeImg} alt="entrada" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saida</p>
+          <img src={outcomeImg} alt="Saida" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  );
+}
