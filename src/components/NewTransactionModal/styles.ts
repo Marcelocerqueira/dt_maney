8ae3f120b67eadf9b51ef60084e0318a127eb3a4
Marcelo cerqueira <marcelocerqueira661@gmail.com>
2021-12-10 23:16:32 -0300

    corrigindo cor

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -11,7 +11,7 @@ export const Container = styled.form`
   input {
     width: 100%;
     padding: 0 1.5rem;
-    height: 44rem;
+    height: 4rem;
     border-radius: 0.25rem;
 
     border: 1px solid #d7d7d7;
